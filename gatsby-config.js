ce96d4f69bd1f5d58f8f6804b20c0b639a16b535
Jamie Burke <jamie@ntslive.co.uk>
2018-02-10 19:03:29 +0000

    Added gatsby sass integration. Removed unused files and default copy.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteMetadata: {
         title: 'Archie Hands',
     },
-    plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sass'
+    ],
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,27 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+import '../../sass/_global.scss'
+
+const websiteName = "Archie Hands";
+const websiteDescription = "WEBSITE DESCRIPTION";
 
 const TemplateWrapper = ({children}) => (
     <div>
         <Helmet
-            title="Gatsby Default Starter"
+            title={websiteName}
             meta={[
-                {name: 'description', content: 'Sample'},
-                {name: 'keywords', content: 'sample, something'},
+                {name: "description", content: websiteDescription},
+                {name: 'keywords', content: ''},
             ]}
         />
-        <Header/>
-        <div
-            style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '0px 1.0875rem 1.45rem',
-                paddingTop: 0,
-            }}
-        >
+        <div>
             {children()}
         </div>
     </div>
