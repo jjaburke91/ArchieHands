d1f3632954737354ec36e5518298ca3f311c5124
Jamie Burke <jamie@ntslive.co.uk>
2018-03-24 17:00:48 +0000

    Implement right banner.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 import ImageBody from '../components/ImageBody';
 import LeftBanner from '../components/LeftBanner';
 
+import '../../sass/right-ban.scss';
+
+import LogoSml from '../images/logo_sml.png';
+
 const IndexPage = () => (
     <div id="page">
         <LeftBanner/>
@@ -11,9 +15,18 @@ const IndexPage = () => (
             <ImageBody />
         </div>
 
-        {/*<div id="right-ban">*/}
-            {/*<div>logo</div>*/}
-        {/*</div>*/}
+        <div className="right-ban">
+            <div className="vertical-absolute-center">
+                <div className="right-ban__logo">
+                    <img src={LogoSml}/>
+                </div>
+
+                <div className="right-ban__controls">
+                    <button className="hands-btn right-ban__controls__next" >next </button>
+                    <button className="hands-btn right-ban__controls__prev" >prev</button>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
