1fc7152338ecb0c8c2f374bedf0fd9ba5b0cc344
Jamie Burke <jjaburke@hotmail.com>
2018-04-08 14:22:33 +0100

    Added socials headers.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,13 +4,17 @@ import Helmet from 'react-helmet'
 
 import '../../sass/style.scss';
 
-const websiteName = "Archie Hands - Designer / Maker";
-const websiteDescription = "Archie Hands - Designer / Maker";
-
 import appleTouchIcon from '../images/favicons/apple-touch-icon.png';
 import faviconSmall from '../images/favicons/favicon-16x16.png';
 import faviconLarge from '../images/favicons/favicon-32x32.png';
 import safariPinnedTab from '../images/favicons/safari-pinned-tab.svg';
+
+const websiteName = "Archie Hands - Designer / Maker";
+const websiteDescription = "Archie Hands - Designer / Maker";
+
+const defaultUrl = "https://www.archiehands.uk";
+const twitterTag = ""; // TODO
+const defaultImageUrl = ""; // TODO
 
 const TemplateWrapper = ({children}) => (
     <div>
@@ -27,6 +31,21 @@ const TemplateWrapper = ({children}) => (
             <link rel="mask-icon" href={safariPinnedTab} color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
+
+            <link rel="canonical" href={defaultUrl} />
+
+            <meta name="description" content={websiteDescription} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content={twitterTag} />
+            <meta name="twitter:image" content={defaultImageUrl} />
+
+            <meta property="og:description" content={websiteDescription} />
+            <meta property="og:site_name" content={websiteName} />
+            <meta property="og:title" content={websiteName} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={defaultUrl} />
+            <meta property="og:image" content={defaultImageUrl} />
+            <meta property="og:image:alt" content={websiteName} />
 
             <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet" />
         </Helmet>
